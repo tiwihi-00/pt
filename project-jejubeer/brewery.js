70251d343abddfd7183b1a42d9a686b8a8ae290b
Your Name <tiwihi@hanmail.net>
2024-11-08 17:54:27 +0900

    1108

--- project-jejubeer/brewery.js
+++ project-jejubeer/brewery.js
@@ -3,3 +3,8 @@ $('span.plus').click(function (e) {
 	$(this).toggleClass('on')
 	$(this).closest('li').find('.item-lists').slideToggle();
 });
+
+gsap.to('.sect_2', {autoAlpha:1})
+const sect2=gsap.timeline();
+
+sect2.
