49eeae9288ede30edd3fc1a2903d8f473121a021
tiwihi-00 <tiwihi@hanmail.met>
2024-12-10 17:43:17 +0900

    1210

--- portfolio/sect3.js
+++ portfolio/sect3.js
@@ -6,60 +6,60 @@
 
 function moveToSelected(element) {
 	var selected;
-	 if (element == "next") {
-		 var selected = $(".selected").next();
-	 } else if (element == "prev") {
-		 var selected = $(".selected").prev();
-	 } else {
-		 var selected = element;
-	 }
- 
-	 var next = $(selected).next();
-	 var prev = $(selected).prev();
-	 var prevSecond = $(prev).prev();
-	 var nextSecond = $(next).next();
- 
-	 $(selected).removeClass().addClass("selected");
- 
-	 $(prev).removeClass().addClass("prev");
-	 $(next).removeClass().addClass("next");
- 
-	 $(nextSecond).removeClass().addClass("nextRightSecond");
-	 $(prevSecond).removeClass().addClass("prevLeftSecond");
- 
-	 $(nextSecond).nextAll().removeClass().addClass('hideRight');
-	 $(prevSecond).prevAll().removeClass().addClass('hideLeft');
- 
- }
- 
- // Eventos teclado
- $(document).keydown(function(e) {
-		 switch(e.which) {
-				 case 37: // left
-				 moveToSelected('prev');
-				 break;
- 
-				 case 39: // right
-				 moveToSelected('next');
-				 break;
- 
-				 default: return;
-		 }
-		 e.preventDefault();
- });
- 
- $('#carousel div').click(function() {
-	 moveToSelected($(this));
- });
- 
- $('#prev').click(function() {
-	 moveToSelected('prev');
- });
- 
- $('#next').click(function() {
-	 moveToSelected('next');
- });
- 
+	if (element == "next") {
+		var selected = $(".selected").next();
+	} else if (element == "prev") {
+		var selected = $(".selected").prev();
+	} else {
+		var selected = element;
+	}
+
+	var next = $(selected).next();
+	var prev = $(selected).prev();
+	var prevSecond = $(prev).prev();
+	var nextSecond = $(next).next();
+
+	$(selected).removeClass().addClass("selected");
+
+	$(prev).removeClass().addClass("prev");
+	$(next).removeClass().addClass("next");
+
+	$(nextSecond).removeClass().addClass("nextRightSecond");
+	$(prevSecond).removeClass().addClass("prevLeftSecond");
+
+	$(nextSecond).nextAll().removeClass().addClass('hideRight');
+	$(prevSecond).prevAll().removeClass().addClass('hideLeft');
+
+}
+
+// Eventos teclado
+$(document).keydown(function (e) {
+	switch (e.which) {
+		case 37: // left
+			moveToSelected('prev');
+			break;
+
+		case 39: // right
+			moveToSelected('next');
+			break;
+
+		default: return;
+	}
+	e.preventDefault();
+});
+
+$('#carousel div').click(function () {
+	moveToSelected($(this));
+});
+
+$('#prev').click(function () {
+	moveToSelected('prev');
+});
+
+$('#next').click(function () {
+	moveToSelected('next');
+});
+
 
 
 
@@ -71,15 +71,15 @@ document.addEventListener('DOMContentLoaded', () => {
 	let tgY = 0;
 
 	const move = () => {
-			curX += (tgX - curX) / 20;
-			curY += (tgY - curY) / 20;
-			interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-			requestAnimationFrame(move);
+		curX += (tgX - curX) / 20;
+		curY += (tgY - curY) / 20;
+		interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+		requestAnimationFrame(move);
 	};
 
 	window.addEventListener('mousemove', (event) => {
-			tgX = event.clientX;
-			tgY = event.clientY;
+		tgX = event.clientX;
+		tgY = event.clientY;
 	});
 
 	move();
